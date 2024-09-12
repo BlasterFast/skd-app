e2b50ebf5b14e644dec359303d96ab04d1174655
Spencer Kenealy <spencerkenealy@Spencers-MBP.lan>
2024-09-11 21:40:46 -0700

    fetching users and logging to console.
    I don't have them showing on screen yet, but will do that next.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,55 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
 
 const linkup = () => {
+  const [usersData, setUsersData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+
+      try {
+        const response = await fetch('https://randomuser.me/api/');
+        if (!response.ok) {
+          throw new Error('Network Response was not okay...');
+        }
+        const jsonData = await response.json();
+        setUsersData(jsonData);
+      } catch (err) {
+        if (err instanceof Error) { 
+          console.log(err.message);
+        }
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <View>
+        <Text>Loading...</Text>
+      </View>
+    )
+  }
+
+  if (error) {
+    return (
+      <View>
+        <Text>Error: {error.message}</Text>
+      </View>
+    )
+  }
+
+  console.log(usersData);
+
   return (
     <View>
       <Text>This is the main LinkUp Match where you match on people. App opens here.</Text>
-      <Text>test</Text>
     </View>
   )
 }
