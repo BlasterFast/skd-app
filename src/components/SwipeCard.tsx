730648b1fef7fda9b4b45938eb311a81159a2c9a
Spencer Kenealy <spencerkenealy@Spencers-MBP.lan>
2024-09-12 17:33:55 -0700

    Further changes before implementing scrollview.
    At this point, I have the user swipe set up real nice with the tan informational section

--- src/components/SwipeCard.tsx
+++ src/components/SwipeCard.tsx
@@ -1,6 +1,6 @@
 import { mainThemeColor } from '@/constants/Colors';
 import React from 'react';
-import { View, Text, Image, StyleSheet, Dimensions } from 'react-native';
+import { View, Text, Image, StyleSheet, Dimensions, ScrollView } from 'react-native';
 import Swiper from 'react-native-deck-swiper';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
@@ -68,14 +68,14 @@ const styles = StyleSheet.create({
   swiperContainer: {
     width: viewportWidth,
     height: viewportHeight * 0.9,
-    backgroundColor: '#fff'
   },
   card: {
     // width: viewportWidth,  // CAUTION This line, and the line below it was making the card hang to the right. Keep for caution.
     // height: viewportHeight * 0.9, 
     // paddingRight: viewportWidth * .107 // this centers the cards instead of them heading right.But it leaves a white line on the card.
     borderRadius: 10,
-    backgroundColor: "#000",
+    backgroundColor: "tan",
+    height: '80%'
   },
   image: {
     width: '100%',
